f66f52c3310d29ea0d726c0e745a99be07a827d1
satyam147 <satyam161211149@gmail.com>
2025-04-03 14:42:26 +0530

    Variables

---
+++ main.js
@@ -0,0 +1,12 @@
+let name = "Your name";
+let num = 1;
+let array = [1, 2, 3];
+let object = {
+  name: "your name",
+  age: 1,
+};
+console.log("log", num);
+num = "new value";
+console.log(num);
+// let body = document.getElementsByTagName("body");
+// body[0].innerHTML = "Your Name";
